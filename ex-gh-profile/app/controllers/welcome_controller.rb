ff671d97c466b1315ad1bd4571c5172e8f38444b
lstravers <stravers222@gmail.com>
2018-08-21 16:33:25 -0400

    modified files.

--- ex-gh-profile/app/controllers/welcome_controller.rb
+++ ex-gh-profile/app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+  
   def index
+    options = {'lstravers': ''}
+    @response = HTTParty.get('https://api.github.com/users/lstravers',options)
   end
+
 end
